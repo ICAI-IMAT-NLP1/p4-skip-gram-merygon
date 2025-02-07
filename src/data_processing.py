4bf03ea6dda64c3027d83ea605ca4f9eea5f3439
MERYGON\meryg <202201175@alu.comillas.edu>
2025-02-07 18:25:16 +0100

    tests de data processing pasados

--- src/data_processing.py
+++ src/data_processing.py
@@ -23,7 +23,8 @@ def load_and_preprocess_data(infile: str) -> List[str]:
 
     # Preprocess and tokenize the text
     # TODO
-    tokens: List[str] = None
+
+    tokens: List[str] = tokenize(text)
 
     return tokens
 
@@ -38,14 +39,19 @@ def create_lookup_tables(words: List[str]) -> Tuple[Dict[str, int], Dict[int, st
         A tuple containing two dictionaries. The first dictionary maps words to integers (vocab_to_int),
         and the second maps integers to words (int_to_vocab).
     """
+    """
+     1. Calculate the frequency of each word in the input list words.
+    2. Sort the vocabulary by frequency, from the most to the least common.
+    3. Generate vocab to int and int to vocab dictionaries for word-integer and integer-word mappings, respectively, using enumeration on the sorted vocabulary.
+    """
     # TODO
-    word_counts: Counter = None
+    word_counts: Counter = Counter(words)
     # Sorting the words from most to least frequent in text occurrence.
-    sorted_vocab: List[int] = None
+    sorted_vocab: List[int] = [word for word, _ in word_counts.most_common()]
     
     # Create int_to_vocab and vocab_to_int dictionaries.
-    int_to_vocab: Dict[int, str] = None
-    vocab_to_int: Dict[str, int] = None
+    int_to_vocab: Dict[int, str] = {idx: word for idx, word in enumerate(sorted_vocab)}
+    vocab_to_int: Dict[str, int] = {word: idx for idx, word in int_to_vocab.items()}
 
     return vocab_to_int, int_to_vocab
 
@@ -69,12 +75,15 @@ def subsample_words(words: List[str], vocab_to_int: Dict[str, int], threshold: f
         List[int]: A list of integers representing the subsampled words, where some high-frequency words may be removed.
         Dict[str, float]: Dictionary associating each word with its frequency.
     """
+
     # TODO
-    # Convert words to integers
-    int_words: List[int] = None
     
-    freqs: Dict[str, float] = None
-    train_words: List[str] = None
+    word_counts: Counter = Counter(words)
+    # Convert words to integers
+    int_words: List[int] = {vocab_to_int[word] for word in words}
+    
+    freqs: Dict[str, float] = {word: count/len(int_words) for word, count in word_counts.items()}
+    train_words: List[int] = [word for word in words if freqs[word] < threshold]
 
     return train_words, freqs
 
@@ -91,11 +100,15 @@ def get_target(words: List[str], idx: int, window_size: int = 5) -> List[str]:
         List[str]: A list of words selected randomly within the window around the target word.
     """
     # TODO
-    target_words: List[str] = None
+    import numpy as np
+    R = np.random.randint(1, window_size + 1)
+    before_words = words[(idx - R):idx]
+    after_words = words[idx+1:(idx + R + 1)]
+    target_words: List[str] = before_words + after_words
 
     return target_words
 
-def get_batches(words: List[int], batch_size: int, window_size: int = 5) -> Generator[Tuple[List[int], List[int]]]:
+def get_batches(words: List[int], batch_size: int, window_size: int = 5):
     """Generate batches of word pairs for training.
 
     This function creates a generator that yields tuples of (inputs, targets),
@@ -113,10 +126,13 @@ def get_batches(words: List[int], batch_size: int, window_size: int = 5) -> Gene
         - The first list contains input words (repeated for each of their context words).
         - The second list contains the corresponding target context words.
     """
-
     # TODO
+    inputs = []
+    targets = []
     for idx in range(0, len(words), batch_size):
-        inputs, targets: Tuple[List[int], List[int]] = None, None
+        for i in range(idx, batch_size):
+            inputs.append(words[idx:i])
+            targets.append(get_target(inputs, i, window_size))
         yield inputs, targets
 
 def cosine_similarity(embedding: torch.nn.Embedding, valid_size: int = 16, valid_window: int = 100, device: str = 'cpu'):
@@ -140,8 +156,34 @@ def cosine_similarity(embedding: torch.nn.Embedding, valid_size: int = 16, valid
         sim = (a . b) / |a||b| where `a` and `b` are embedding vectors.
     """
 
+    """
+    • Use a PyTorch Embedding module, which contains the word vectors learned by the Skip-gram model.
+    • Select a subset of words (valid size) from within a specified range (valid window) to serve as
+    your basis for comparison. These words are randomly chosen to represent a diverse set of points
+    in the embedding space, as evaluating this function for all the word embeddings would result in an
+    excessive computational cost.
+
+    • Calculate the cosine similarity between the embeddings of the selected words and all other words in
+    the vocabulary. The cosine similarity metric will help you understand the degree of similarity, with
+    a value close to 1 indicating high similarity and a value close to-1 indicating high dissimilarity.
+    Remember, cosine similarity is given by:
+    cosine similarity(A,B) = A · B
+    ∥A∥∥B∥
+    where:
+    • Aand B are vectors.
+    • A·B denotes the dot product of vectors.
+    • ∥A∥ and ∥B∥ denote the Euclidean norms of vectors A and B, respectively
+    """
+
     # TODO
-    valid_examples: torch.Tensor = None
-    similarities: torch.Tensor = None
+    embeddings = embedding.weight.to(device)
+
+    valid_examples: torch.Tensor = torch.randint(0, valid_window, (valid_size,), device=device)
+    similarities: torch.Tensor = torch.zeros(valid_size, len(embeddings), device=device)
+    
+    for i, valid_idx in enumerate(valid_examples):
+        valid_embedding = embeddings[valid_idx].unsqueeze(0)
+        similarity = torch.nn.functional.cosine_similarity(valid_embedding, embeddings)
+        similarities[i] = similarity
 
     return valid_examples, similarities
